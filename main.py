c7a8737d44792386e767fa3eee3f4e598e434165
mrcatcis <domozhirov.anton2015@ya.ru>
2021-03-12 15:08:41 +0300

    finally

--- main.py
+++ main.py
@@ -10,46 +10,75 @@ class Window(QWidget):
 
     def initUI(self):
         grid = QGridLayout()
-        self.setLayout(grid)
 
-        self.addButton = QPushButton('+', self)
-        self.addButton.clicked.connect(self.add)
-        grid.addWidget(self.addButton, 0, 1)
+        addButton = QPushButton('+', self)
+        addButton.clicked.connect(self.add)
+        grid.addWidget(addButton, 0, 1)
 
-        self.subButton = QPushButton('-', self)
-        self.subButton.clicked.connect(self.sub)
-        grid.addWidget(self.subButton, 1, 1)
+        subButton = QPushButton('-', self)
+        subButton.clicked.connect(self.sub)
+        grid.addWidget(subButton, 1, 1)
 
-        self.mulButton = QPushButton('*', self)
-        self.mulButton.clicked.connect(self.mul)
-        grid.addWidget(self.mulButton, 2, 1)
+        mulButton = QPushButton('*', self)
+        mulButton.clicked.connect(self.mul)
+        grid.addWidget(mulButton, 2, 1)
 
-        self.divButton = QPushButton('/', self)
-        self.divButton.clicked.connect(self.div)
-        grid.addWidget(self.divButton, 3, 1)
+        divButton = QPushButton('/', self)
+        divButton.clicked.connect(self.div)
+        grid.addWidget(divButton, 3, 1)
 
         self.var1 = QLineEdit(self)
         grid.addWidget(self.var1, 1, 0)
         self.var2 = QLineEdit(self)
         grid.addWidget(self.var2, 2, 0)
 
+        self.ans = QLabel('asd', self)
+        grid.addWidget(self.ans, 4, 0, 1, 2, Qt.AlignCenter)
+
+
+        self.setLayout(grid)
         self.show()
+
+    def get_arguments(self):
+        a = self.var1.text()
+        b = self.var2.text()
+        if not(a.isdigit() and b.isdigit()):
+            raise TypeError('not an integers')
+        return int(a), int(b)
 
     @pyqtSlot()
     def add(self):
-        pass
+        try:
+            a, b = self.get_arguments()
+            self.ans.setText(str(f'{a} + {b} = {a+b}'))
+        except Exception as e:
+            self.ans.setText(*e.args)
 
     @pyqtSlot()
     def sub(self):
-        pass
+        try:
+            a, b = self.get_arguments()
+            self.ans.setText(str(f'{a} - {b} = {a-b}'))
+        except Exception as e:
+            self.ans.setText(*e.args)
 
     @pyqtSlot()
     def mul(self):
-        pass
+        try:
+            a, b = self.get_arguments()
+            self.ans.setText(str(f'{a} * {b} = {a*b}'))
+        except Exception as e:
+            self.ans.setText(*e.args)
 
     @pyqtSlot()
     def div(self):
-        pass
+        try:
+            a, b = self.get_arguments()
+            self.ans.setText(str(f'{a} / {b} = {a/b}'))
+        except Exception as e:
+            self.ans.setText(*e.args)
+        except ZeroDivisionError as e:
+            self.ans.setText('second var is 0')
 
 
 if __name__ == '__main__':
